bcd769aa7eb737d67d7ac4a55ef8ea56e2e75dda
nehaaror <72908070+nehaaror@users.noreply.github.com>
2021-04-16 03:40:58 +0530

    Course services, Interfaces ,repository,Viewmodels

---
+++ Solution1/CleanarchApplcation/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using CleanarchApplcation.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanarchApplcation.Interfaces
+{
+   public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
