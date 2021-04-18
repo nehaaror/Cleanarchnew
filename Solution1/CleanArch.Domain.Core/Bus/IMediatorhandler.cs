0614bc4668f52422e746a6f2292a78696abe95af
nehaaror <72908070+nehaaror@users.noreply.github.com>
2021-04-19 02:57:34 +0530

    adding domain.core and implementation of nmemory.bus

---
+++ Solution1/CleanArch.Domain.Core/Bus/IMediatorhandler.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Domain.Core.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Core.Bus
+{
+    public interface IMediatorhandler
+    {
+        public Task Sendcommand<T>(T command) where T : Command;
+        
+    }
+}
--- Solution1/CleanArch.infra.bus/InMemorybus.cs
+++ Solution1/CleanArch.infra.bus/InMemorybus.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using CleanArch.Domain.Core.Bus;
+using CleanArch.Domain.Core.Commands;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CleanArch.infra.bus
 {
-    class InMemorybus
+   public sealed class InMemorybus : IMediatorhandler
     {
+        private readonly IMediator _mediator;
+        public InMemorybus( IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    
+        public Task Sendcommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
